32b28f24b5815e0d5f11199a439138adb4493ddf
Alexandr Gopienko <sagopien04@mail.ru>
2023-03-07 11:18:12 +0700

    'project'

--- math_app.py
+++ math_app.py
@@ -9,9 +9,19 @@ def update_figure(data):
     axes[0].plot(x, y, 'r-')
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack()
-    
+
+def input_check(indut_data):
+    for symb in new_value:
+        if not symb in avalaible_symbols:
+            return False
+        else:
+            return True
+            
 sg.theme('DarkTeal6')
 table_content = []
+avalaible_symbols = ('x', ' ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '-', '+', '*',
+                     '^', 'exp', '(', ')', 'sqrt')
+x = 0
 layout = [
     [sg.Table(headings = ['Observation', 'Result'],
               values = table_content,
@@ -19,12 +29,12 @@ layout = [
               hide_vertical_scroll=True,
               key = '-TABLE-')],
     [sg.Input(key = '-INPUT-', expand_x = True), sg.Button('Submit')],
-    [sg.Canvas(key = '-CANVAS-')],
+    [sg.Canvas(key = '-CANVAS-')]
 
 ]
 
 window = sg.Window('Graph App', layout, finalize=True)
-sg.theme('DarkTeal6')
+
 fig = Figure(figsize = (5,4))
 fig.add_subplot(111).plot([],[])
 figure_canvas_agg = FigureCanvasTkAgg(fig, window['-CANVAS-'].TKCanvas)
@@ -37,11 +47,17 @@ while True:
         break
     if event == 'Submit':
         new_value = values["-INPUT-"]
-        if new_value.isnumeric():
+        if input_check(new_value):
+            for elem in new_value:
+                if elem == 'x':
+                    pass
+                elif elem.isdigit():
+                    pass
+                elif elem == (' ', '(', ')', '+', '*', '-'):
+                    pass
             table_content.append([len(table_content) +1 , float(new_value)])
             window['-TABLE-'].update(table_content)
             window['-INPUT-'].update('')
             update_figure(table_content)
-
 
 window.close()
